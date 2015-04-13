c40d4b0769fac40156d24813149f92b8ce7dcb41
opedromiranda <p.miguel.tr@gmail.com>
2015-04-13 22:21:33 +0100

    setting JsonProperties for Username and Password

--- TDIN-Diginote/Models/User.cs
+++ TDIN-Diginote/Models/User.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 namespace TDIN_Diginote.Models
 {
     class User
     {
+        [JsonProperty(PropertyName = "username")]
         public String Username;
+
+        [JsonProperty(PropertyName = "password")]
         public String Password;
 
         public User(String username, String password)
